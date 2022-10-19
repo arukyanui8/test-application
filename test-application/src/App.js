2d722561c135be32c1e27e4f9c3666ea350f31ec
arukyanui8 <xarukyan@gmail.com>
2022-10-19 05:30:22 +0400

    first commit react

---
+++ test-application/src/App.js
@@ -0,0 +1,41 @@
+import {React, useState}  from 'react'
+import "./App.css"
+ 
+export default function App() {
+  let [counter, setCounter] = useState(0);
+
+  //Incrementation
+  const incrementation = () => {
+    setCounter(counter => counter + 1)
+  }
+
+  //Incrementation
+  const decrementation = () => {
+    setCounter(counter => counter - 1)
+  }
+
+  //Reset
+  const reset = () => {
+    setCounter(counter = 0)
+    console.log(counter)
+  }
+
+  return (
+    <div id='container'>
+      <div className="box">
+        <div className='info_block'>
+          <h1>Play this game</h1>
+          <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, aut! Nobis nostrum et suscipit, culpa sapiente porro quia voluptas ipsam iure nulla perspiciatis quibusdam, a obcaecati labore voluptatum quidem quasi animi. Illum, accusantium. Dolores sed blanditiis alias consequatur quasi quo harum totam earum velit qui. </p>
+        </div>
+        <span className="counter_output">{counter}</span>
+        <div className="counter_actions">
+          <button className='primary_button' onClick={decrementation}>Decrement</button>
+          <button className="primary_button" onClick={incrementation}>Increment</button>
+        </div>
+        <button className='icon_button' onClick={reset}>Reset</button>
+
+      </div>
+  </div>
+    
+  );
+}
