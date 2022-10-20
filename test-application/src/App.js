6a52c777315592080ea2cec72fab95fa3cecaadf
arukyanui8 <xarukyan@gmail.com>
2022-10-20 19:00:19 +0400

    seconda commit

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -3,17 +3,19 @@ import "./App.css"
  
 export default function App() {
   let [counter, setCounter] = useState(0);
+  const [color, setColor] = useState('blue');
 
   //Incrementation
   const incrementation = () => {
-    setCounter(counter => counter + 1)
+    setCounter(counter += 1)
+    setColor(color => color = 'green')
   }
 
-  //Incrementation
+  //Decrementation
   const decrementation = () => {
-    setCounter(counter => counter - 1)
+    setCounter(counter -= 1)
+    setColor(color => color = 'red')
   }
-
   //Reset
   const reset = () => {
     setCounter(counter = 0)
@@ -27,7 +29,7 @@ export default function App() {
           <h1>Play this game</h1>
           <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, aut! Nobis nostrum et suscipit, culpa sapiente porro quia voluptas ipsam iure nulla perspiciatis quibusdam, a obcaecati labore voluptatum quidem quasi animi. Illum, accusantium. Dolores sed blanditiis alias consequatur quasi quo harum totam earum velit qui. </p>
         </div>
-        <span className="counter_output">{counter}</span>
+        <span className="counter_output" style={{color}}>{counter}</span>
         <div className="counter_actions">
           <button className='primary_button' onClick={decrementation}>Decrement</button>
           <button className="primary_button" onClick={incrementation}>Increment</button>
