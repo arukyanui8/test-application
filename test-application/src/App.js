bccb2a085c631c1dde4f33342d77ef1167092116
arukyanui8 <xarukyan@gmail.com>
2022-10-22 15:02:06 +0400

    added component checker

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -1,25 +1,21 @@
-import {React, useState}  from 'react'
+import React,{useState}  from 'react'
 import "./App.css"
  
 export default function App() {
   let [counter, setCounter] = useState(0);
-  const [color, setColor] = useState('blue');
 
   //Incrementation
   const incrementation = () => {
     setCounter(counter += 1)
-    setColor(color => color = 'green')
   }
 
   //Decrementation
   const decrementation = () => {
     setCounter(counter -= 1)
-    setColor(color => color = 'red')
   }
   //Reset
   const reset = () => {
     setCounter(counter = 0)
-    console.log(counter)
   }
 
   return (
@@ -29,12 +25,11 @@ export default function App() {
           <h1>Play this game</h1>
           <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, aut! Nobis nostrum et suscipit, culpa sapiente porro quia voluptas ipsam iure nulla perspiciatis quibusdam, a obcaecati labore voluptatum quidem quasi animi. Illum, accusantium. Dolores sed blanditiis alias consequatur quasi quo harum totam earum velit qui. </p>
         </div>
-        <span className="counter_output" style={{color}}>{counter}</span>
+        <span className="counter_output">{counter}</span>
         <div className="counter_actions">
           <button className='primary_button' onClick={decrementation}>Decrement</button>
           <button className="primary_button" onClick={incrementation}>Increment</button>
         </div>
-        <button className='icon_button' onClick={reset}>Reset</button>
         <button className='icon_button' onClick={reset}>Reset</button>
       </div>
   </div>
---
+++ test-application/src/components/ Checker/Checker.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Checker.scss"
+
+export default function Checker() {
+    return (
+        <div id='container'></div>
+    );
+}
