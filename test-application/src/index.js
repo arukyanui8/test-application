d92948557e409b42a93cd617687d94d96116a907
arukyanui8 <xarukyan@gmail.com>
2022-10-30 17:13:35 +0400

    added donts folder

--- test-application/src/index.js
+++ test-application/src/index.js
@@ -6,6 +6,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <App />
+    <App />
+    <App />
+    <App />
+    <App />
   </React.StrictMode>
 );
 
